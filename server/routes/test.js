530ea449e227ccf0700a4c5652f98ae84e9c17c8
AtlasX1 <pavlo.sergij@ukr.net>
2021-04-26 21:03:24 +0300

    try fix

--- server/routes/test.js
+++ server/routes/test.js
@@ -1,39 +1,30 @@
-// const Test = require("../models/test");
+const Test = require("../models/test");
 const router = require("express").Router();
 
-// router.post("/create", async (req, res) => {
-//   try {
-//     const test = new Test({
-//       name: "test name",
-//       number: 2,
-//     });
-
-//     await test.save();
-//     return res
-//       .status(200)
-//       .json({ message: "Test created successfully!", task });
-//   } catch (e) {
-//     console.log(e);
-//     return res.status(500).json({ message: "Something went wrong, try again" });
-//   }
-// });
-
-// router.get("/", async (req, res) => {
-//   try {
-//     const tests = await Test.find({});
-//     if (!tests) {
-//       return res.status(400).json({ message: "tests not found, try again" });
-//     }
-//     return res.status(200).json({ tests });
-//   } catch (e) {
-//     console.log(e);
-//     return res.status(500).json({ message: "Something went wrong, try again" });
-//   }
-// });
-
-router.get("/", async () => {
+router.post("/create", async (req, res) => {
   try {
-    return res.status(200).json({ ok: "ok" });
+    const test = new Test({
+      name: "test name",
+      number: 2,
+    });
+
+    await test.save();
+    return res
+      .status(200)
+      .json({ message: "Test created successfully!", task });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: "Something went wrong, try again" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const tests = await Test.find({});
+    if (!tests) {
+      return res.status(400).json({ message: "tests not found, try again" });
+    }
+    return res.status(200).json({ tests });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ message: "Something went wrong, try again" });
