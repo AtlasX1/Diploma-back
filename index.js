126b293d891d4718997ac3faf0d7830c77cad859
AtlasX1 <pavlo.sergij@ukr.net>
2021-04-26 20:30:19 +0300

    init project

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+
+require("dotenv").config();
+const initServer = () => {
+  const app = express();
+  const bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.get("/", (req, res) => {
+    res.end("Server is running!");
+  });
+
+  app.use("/api/auth", require("./server/routes/auth.routes"));
+  app.use("/api/tasks", require("./server/routes/tasks.routes"));
+  app.use("/api/userTries", require("./server/routes/userTries.routes"));
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("Server running");
+  });
+};
+mongoose.connect(
+  process.env.MONGO_DB,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  () => {
+    console.log("connected to db");
+
+    initServer();
+  }
+);
