1389ed98e20f67f7c9d16476d99d528deefaa121
AtlasX1 <pavlo.sergij@ukr.net>
2021-05-11 14:48:13 +0300

    fix cors

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const cors = require("cors");
+var allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
 
+  // intercept OPTIONS method
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
 require("dotenv").config();
 const initServer = () => {
   const app = express();
-  app.use(cors());
+  app.configure(function () {
+    app.use(allowCrossDomain);
+    // app.use(express.bodyParser());
+  });
   const bodyParser = require("body-parser");
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
