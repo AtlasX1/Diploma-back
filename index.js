3f2380c65685defa1c8d1ecb723bbe173b8f3749
AtlasX1 <pavlo.sergij@ukr.net>
2021-05-11 14:45:33 +0300

    fix cors

--- index.js
+++ index.js
@@ -6,32 +6,15 @@ const cors = require("cors");
 require("dotenv").config();
 const initServer = () => {
   const app = express();
-  app.use(cors({ origin: "*" }));
+  app.use(cors());
   const bodyParser = require("body-parser");
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(express.json());
-
-  app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
-
-    res.setHeader("Access-Control-Allow-Credentials", true);
-
-    next();
-  });
   app.get("/", (req, res) => {
     res.end("Server is running!");
   });
+
   // app.use("/api/test", require("./server/routes/test"));
   app.use("/api/substance", require("./server/routes/substance"));
   app.use("/api/substance", require("./server/routes/getInfo"));
